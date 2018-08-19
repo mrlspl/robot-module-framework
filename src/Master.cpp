a265c26fdf8aa37c101d587a8ec172fe7bebc49f
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2018-08-19 19:40:42 +0430

    Add a cpp file!

---
+++ src/Master.cpp
@@ -0,0 +1,27 @@
+
+#include "Master.hpp"
+
+using namespace RMF;
+
+void Master::registerNode()
+{
+
+}
+
+void Master::registerPublisher()
+{
+
+}
+
+void Master::registerSubscriber()
+{
+
+}
+
+void Master::update()
+{
+
+}
+
+
+#endif //ROBOT_MODULE_FRAMEWORK_MASTER_HPP
